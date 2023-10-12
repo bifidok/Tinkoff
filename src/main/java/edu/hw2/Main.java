2ef5830f2220625bf86447b01b7c587e55eac2e3
Alexander <alexander.s3547@gmail.com>
2023-10-12 09:56:01 +0700

    add 1 Task

--- src/main/java/edu/hw2/Main.java
+++ src/main/java/edu/hw2/Main.java
@@ -1,5 +1,6 @@
 package edu.hw2;
 
+import edu.hw2.Task1.Expr;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,5 +11,14 @@ public final class Main {
     }
 
     public static void main(String[] args) {
+        var two = new Expr.Constant(2);
+        var four = new Expr.Constant(4);
+        var negOne = new Expr.Negate(new Expr.Constant(1));
+        var sumTwoFour = new Expr.Addition(two, four);
+        var mult = new Expr.Multiplication(sumTwoFour, negOne);
+        var exp = new Expr.Exponent(mult, 2);
+        var res = new Expr.Addition(exp, new Expr.Constant(1));
+
+        System.out.println(res + " = " + res.evaluate());
     }
 }
