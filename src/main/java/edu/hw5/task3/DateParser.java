daec9722a51b0161028bc8871c931ee2a2ac5aaa
Alexander <alexander.s3547@gmail.com>
2023-11-21 21:58:48 +0700

    review changes

--- src/main/java/edu/hw5/task3/DateParser.java
+++ src/main/java/edu/hw5/task3/DateParser.java
@@ -4,19 +4,19 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 public class DateParser {
+    private final static MiddleWare MIDDLE_WARE = MiddleWare.link(
+        new Parser1(),
+        new Parser2(),
+        new Parser3(),
+        new Parser4(),
+        new Parser5(),
+        new Parser6()
+    );
 
     private DateParser() {
     }
 
     public static Optional<LocalDate> parseDate(String string) {
-        MiddleWare middleWare = MiddleWare.link(
-            new Parser1(),
-            new Parser2(),
-            new Parser3(),
-            new Parser4(),
-            new Parser5(),
-            new Parser6()
-        );
-        return middleWare.parseDate(string);
+        return MIDDLE_WARE.parseDate(string);
     }
 }
